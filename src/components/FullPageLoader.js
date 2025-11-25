ecd8ec0ae70533b97ef8682fdd84a1aba696e8d1
commit-panda <sjmanyarkiy@gmail.com>
2025-11-25 16:25:58 +0300

    fixed spinner

---
+++ src/components/FullPageLoader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Oval } from 'react-loader-spinner'
+import { OrbitProgress } from 'react-loading-indicators'
+
+function FullPageLoader() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FullPageLoader
