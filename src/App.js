0ac7750d35e338856a1264aa6a539ede8a917e7a
commit-panda <sjmanyarkiy@gmail.com>
2025-11-25 15:27:53 +0300

    adding landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
+import TestHome from './pages/TestHome';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      {/* <TestHome /> */}
 
     </div>
   );
